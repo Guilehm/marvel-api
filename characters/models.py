3eac04e4c5fab6357c11076df8676c82c8b63352
Guilherme Luis <guile.hm@hotmail.com>
2019-02-13 21:25:19 -0200

    Create character model

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=512, null=True, blank=True)
+    resource_uri = models.URLField(null=True, blank=True)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_changed = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
