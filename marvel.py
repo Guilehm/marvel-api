2d4903cf492041dca15f7283b8e6290c6901b6cf
Guilherme <guile.hm@hotmail.com>
2019-02-12 23:17:34 -0200

    create marvel file

---
+++ marvel.py
@@ -0,0 +1,13 @@
+import hashlib
+from datetime import datetime
+
+
+class Marvel:
+    def __init__(self, private_key, public_key):
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def get_hash(self):
+        timestamp = datetime.now().timestamp()
+        formatted_string = f'{timestamp}{self.private_key}{self.public_key}'
+        return hashlib.md5(formatted_string.encode('utf-8')).hexdigest()
