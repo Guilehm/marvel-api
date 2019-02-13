9ba52adc2cba388739bc3e1d335a7502a8fbb72b
Guilherme Luis <guile.hm@hotmail.com>
2019-02-13 00:02:52 -0200

    create send request method and improve get auth data method

--- marvel.py
+++ marvel.py
@@ -1,14 +1,22 @@
 import hashlib
 from datetime import datetime
+import requests
 
 
 class Marvel:
-    def __init__(self, private_key, public_key):
+    def __init__(self, private_key, public_key, api_version='v1'):
         self.private_key = private_key
         self.public_key = public_key
-        self.url = 'http://gateway.marvel.com/v1/public/'
+        self.base_url = f'http://gateway.marvel.com/{api_version}/public/'
 
-    def get_hash(self):
+    def get_auth_data(self):
         timestamp = datetime.now().timestamp()
         formatted_string = f'{timestamp}{self.private_key}{self.public_key}'
-        return hashlib.md5(formatted_string.encode('utf-8')).hexdigest()
+        hashed_data = hashlib.md5(formatted_string.encode('utf-8')).hexdigest()
+        data = dict(ts=timestamp, apikey=self.public_key, hash=hashed_data)
+        return data
+
+    def send_request(self, endpoint_path):
+        url = f'{self.base_url}{endpoint_path}'
+        auth_data = self.get_auth_data()
+        return requests.get(url, params=auth_data)
