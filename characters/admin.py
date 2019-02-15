a50cd157877c4e72d7131f5016fabbefe4ac613f
Guilherme Luis <guile.hm@hotmail.com>
2019-02-15 21:03:50 -0200

    create comic item admin

--- characters/admin.py
+++ characters/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from characters.models import Character
+from characters.models import Character, ComicItem
 
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id')
+    list_display = ('id', 'name',)
+    list_filter = ('date_added', 'date_changed')
+    search_fields = ('id', 'name', 'resource_uri')
+
+
+@admin.register(ComicItem)
+class ComicItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
     list_filter = ('date_added', 'date_changed')
     search_fields = ('id', 'name', 'resource_uri')
