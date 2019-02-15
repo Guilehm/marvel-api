fc46f847eb4c9a7df38e122df83f15a3864b7607
Guilherme Luis <guile.hm@hotmail.com>
2019-02-14 23:29:13 -0200

    Improve characters admin

--- characters/admin.py
+++ characters/admin.py
@@ -5,6 +5,6 @@ from characters.models import Character
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'id')
     list_filter = ('date_added', 'date_changed')
-    search_fields = ('name', 'resource_uri')
+    search_fields = ('id', 'name', 'resource_uri')
