e07e4b2e99eac1ad30b745f43b36571b4fad9139
Guilherme Luis <guile.hm@hotmail.com>
2019-02-16 22:40:52 -0300

    Add settings for debug toolbar

--- marvel/settings_dev.py
+++ marvel/settings_dev.py
@@ -7,4 +7,11 @@ ALLOWED_HOSTS += [
 
 INSTALLED_APPS += [
     'django_extensions',
+    'debug_toolbar',
 ]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1']
